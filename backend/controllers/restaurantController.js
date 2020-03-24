57b7c259763046aac2087758a7bce97ec9b2af05
Denise Cheung <denisecheung33@gmail.com>
2020-03-24 20:18:20 +0000

    save

--- backend/controllers/restaurantController.js
+++ backend/controllers/restaurantController.js
@@ -60,8 +60,60 @@ function editARestaurant(req, res) {
 }
 
 function CreateNewComment(req, res) {
+  const currentUser = req.currentUser
+  req.body.user = currentUser // really important line, this is essentially adding a user field to our req.body (in JSON left side in insomia)
+  Restaurant
+    .findById(req.params.id)
+    .then(restaurant => {
+      if (!restaurant) return res.status(404).send({ message: 'No restaurant with this ID, sadly.' })
+      restaurant.comments.push(req.body)
+      return restaurant.save()
+    })
+    .then(restaurant => res.status(201).send(restaurant))
+    .catch(error => res.send(error))
+}
 
+function EditAComment(req, res) {
+  const currentUser = req.currentUser
+  Restaurant
+    .findById(req.params.id)
+    .then(restaurant => {
+      if (!restaurant) return res.status(404).send({ message: 'No restaurant with this ID' })
 
+      //at this point we have the specific resto, now get the comment I want to edit 
+      const comment = restaurant.comments.id(req.params.commentId)
+
+      //check if the comment is posted by the same person who is now trying to edit it 
+      if (!comment.user.equals(currentUser._id)) {
+        return res.status(401).send({ message: 'Do not edit others restaurants!' })
+      }
+      //at this point it would be the same person, so we want to update the resto with the new text (on left side in insomia, json body)
+      comment.set(req.body)
+      return restaurant.save()
+    })
+    .then(restaurant => res.status(200).send(restaurant))
+    .catch(error => res.send(error))
+
+}
+
+function DeleteAComment(req, res) {
+  const currentUser = req.currentUser
+  Restaurant
+    // id refers to the resto ID that the comment lives on
+    .findById(req.params.id)
+    .then(restaurant => {
+      if (!restaurant) return res.status(404).send({ message: 'No restaurant with this ID' })
+      // get the comment that I need to delete. We get it using the id method of mongoose 
+      const comment = restaurant.comments.id(req.params.commentId)
+      if (!comment.user.equals(currentUser._id)) {
+        return res.status(401).send({ message: 'Do not delete other peoples restaurant!' })
+      }
+      // Mongoose Remove method to delete that comment
+      comment.remove()
+      return restaurant.save()
+    })
+    .then(restaurant => res.status(202).send(restaurant)) //send back the resto
+    .catch(error => res.send(error))
 }
 
 
@@ -71,5 +123,7 @@ module.exports = {
   deleteARestaurant,
   viewARestaurant,
   editARestaurant,
-  CreateNewComment
+  CreateNewComment,
+  EditAComment,
+  DeleteAComment
 }
