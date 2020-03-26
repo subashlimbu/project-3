344b19e68defcc78b94a866f9a5928dfcab9a3ce
Denise Cheung <denisecheung33@gmail.com>
2020-03-26 20:56:00 +0000

    styled login page

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import axios from 'axios'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSignInAlt } from '@fortawesome/free-solid-svg-icons'
+
+
 
 import auth from '../lib/auth'
 
@@ -39,48 +43,63 @@ class Login extends React.Component {
 
   render() {
     const { error } = this.state
-    return <section className="section">
-      <div className="container">
-        <h1 className="title">Login</h1>
-        <form
-          className="form"
-          onSubmit={(event) => this.handleSubmit(event)}
-        >
-          <div className="field">
-            <label className="label">
-              Email
-            </label>
-            <div className="control">
-              <input
-                onChange={(event) => this.handleChange(event)}
-                type="text"
-                name="email"
-                className="input"
-              />
+    return <section className="hero is-success is-fullheight">
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          <div className="column is-4 is-offset-4">
+            <h1 className="title">Login</h1>
+            <hr className="login-hr" />
+            <p className="subtitle">Please login to proceed.</p>
+            <div className="box">
+              <figure className="avatar">
+                <img src="https://www.astleypark.lancs.sch.uk/images/blog/welcome-back.jpg" />
+              </figure>
+              <form
+                className="form"
+                onSubmit={(event) => this.handleSubmit(event)}
+              >
+                <div className="field">
+                  <label className="label">
+                    Email
+                  </label>
+                  <div className="control">
+                    <input
+                      onChange={(event) => this.handleChange(event)}
+                      type="text"
+                      name="email"
+                      className="input"
+
+                    />
+                  </div>
+                </div>
+
+                <div className="field">
+                  <label className="label">
+                    Password
+                  </label>
+                  <div className="control">
+                    <input
+                      onChange={(event) => this.handleChange(event)}
+                      type="password"
+                      name="password"
+                      className="input"
+                    />
+                  </div>
+                  {error && <small className="help is-danger">
+                    {error}
+                  </small>}
+                </div>
+
+                <button className="button is-success is-block is-large is-fullwidth">
+
+                  Login
+                  <FontAwesomeIcon icon={faSignInAlt} />
+                  {/* <FontAwesomeIcon icon={faThumbsUp} /> */}
+                </button>
+              </form>
             </div>
           </div>
-
-          <div className="field">
-            <label className="label">
-              Password
-            </label>
-            <div className="control">
-              <input
-                onChange={(event) => this.handleChange(event)}
-                type="password"
-                name="password"
-                className="input"
-              />
-            </div>
-            {error && <small className="help is-danger">
-              {error}
-            </small>}
-          </div>
-
-          <button className="button is-success">
-            Login
-          </button>
-        </form>
+        </div>
       </div>
     </section>
   }
