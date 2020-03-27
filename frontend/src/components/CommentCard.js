08d84a7f8e137844d0e6a9ef191a5d9f7f1545f2
subashlimbu <prajan_l@hotmail.com>
2020-03-27 16:22:37 +0000

    day4

--- frontend/src/components/CommentCard.js
+++ frontend/src/components/CommentCard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Axios from 'axios';
 import auth from '../lib/auth';
+import moment from 'moment'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faThumbsUp, faThumbsDown } from '@fortawesome/free-solid-svg-icons'
@@ -76,19 +77,23 @@ class CommentCard extends React.Component {
 
   render() {
     const user = auth.getName()
+    const isloggedIn = auth.isLoggedIn()
     const { isLiked, isDisliked } = this.state
     const { comment } = this.props
+    console.log(this.props)
 
     return <div className="comment">
       <h2>{comment.user.username}</h2>
-      <p>{comment.text}</p>
+      <p><strong>{comment.text}</strong></p>
+      <p className="time">{moment(comment.createdAt).format('DD/MM/YYYY')}</p>
       <i className="far fa-thumbs-up"></i>
       <i className="far fa-thumbs-down"></i>
       <span>{comment.likedBy.length}</span>
       <FontAwesomeIcon className={isLiked ? 'liked' : ''} icon={faThumbsUp} onClick={() => this.handleLike()} />
       <span>{comment.dislikedBy.length}</span>
       <FontAwesomeIcon className={isDisliked ? 'disliked' : ''} icon={faThumbsDown} onClick={() => this.handleDislike()} />
-      {user === comment.user.username && <button className="button is-danger" onClick={this.props.onClick}>Delete</button>}
+      {isloggedIn && user === comment.user.username && <button className="button is-danger is-round" onClick={this.props.onClick}>Delete</button>}
+      <hr />
     </div>
   }
 }
